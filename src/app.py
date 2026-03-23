28037b911ff7d20a8d6e08df66c63fb423582f5b
Robb <rfao300@hotmail.com>
2026-03-23 21:27:56 +0000

    First Commit

--- src/app.py
+++ src/app.py
@@ -28,15 +28,44 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
+# Todos los miembros
+
 
 @app.route('/members', methods=['GET'])
-def handle_hello():
+def handle_get_all_members():
     # This is how you can use the Family datastructure by calling its methods
     members = jackson_family.get_all_members()
-    response_body = {"hello": "world",
-                     "family": members}
-    return jsonify(response_body), 200
+    return jsonify(members), 200
 
+# Miembro individual
+@app.route('/members/<int:id>', methods=['GET'])
+def handle_get_single(id):
+    member = jackson_family.get_member(id)
+    if member:
+        return jsonify(member), 200
+    return jsonify({"msg": "Member not found"}), 404
+
+# Agregar member
+@app.route('/members', methods=['POST'])
+def handle_add():
+    new_member = request.get_json()
+
+    if not new_member:
+        return jsonify({"msg": "Body is required"}), 400
+
+    # Guarda el member
+    added_member = jackson_family.add_member(new_member)
+
+    return jsonify(added_member), 200
+
+
+# Eliminar member
+@app.route('/members/<int:id>', methods=['DELETE'])
+def handle_delete(id):
+    success = jackson_family.delete_member(id)
+    if success:
+        return jsonify({"done": True}), 200
+    return jsonify({"msg": "Member not found"}), 404
 
 
 # This only runs if `$ python src/app.py` is executed
